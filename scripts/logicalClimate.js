1dd92bf143b21b66172b70b7e956ec051ad4cced
Pedro Paulo Daris <97978711+Dariszz@users.noreply.github.com>
2022-02-10 19:59:06 -0300

    Fix: fixing  images paths

--- scripts/logicalClimate.js
+++ scripts/logicalClimate.js
@@ -11,17 +11,17 @@ export default function logicalClimate() {
         let icons = data.current.condition.text;
 
         if (icons.includes('thunder') && icons.includes('rain')){
-            addIconClimate = "../assets/icons/storm.png";            
+            addIconClimate = "./assets/icons/storm.png";            
         }else if (icons.includes('thunder')) {
-            addIconClimate = "../assets/icons/thunder.png";
+            addIconClimate = "./assets/icons/thunder.png";
         }else if (icons.includes('rain') || icons.includes('drizzle')) {
-            addIconClimate = "../assets/icons/raining.png";
+            addIconClimate = "./assets/icons/raining.png";
         }else if (icons.includes('Fog') || icons.includes('Misty')) {
-            addIconClimate = "../assets/icons/cloud.png";
+            addIconClimate = "./assets/icons/cloud.png";
         }else if (icons.includes('Cloudy') || icons.includes('Overcast')) {
-            addIconClimate = "../assets/icons/cloudy.png";
+            addIconClimate = "./assets/icons/cloudy.png";
         }else if (icons.includes('Sunny')) {
-            addIconClimate = "../assets/icons/sun.png";
+            addIconClimate = "./assets/icons/sun.png";
         } 
 
         iconClimate.src = addIconClimate;
