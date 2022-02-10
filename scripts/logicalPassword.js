e2dd8bb2ca505380bf8a9e6fb11dc759cd7d828b
Pedro Paulo Daris <pedropaulobmdaris@gmail.com>
2022-02-10 13:10:25 -0300

    Feat: adding js to the loginpage

---
+++ scripts/logicalPassword.js
@@ -0,0 +1,29 @@
+import logicalTimer from "./helpers/logicalTimer.js";
+
+export default function logicalPassword(){
+    const $ = document.querySelector.bind(document);
+    const userLogin = $('#placeholderLogin');
+    const inputLogin = $('#inputLogin');
+    const userPassword = $('#placeholderPassword');
+    const inputPassword = $('#inputPassword');
+    const button = $('#button');
+    const messageError = $('#messageError');
+    const loginPage = $('#loginPage');
+    const homePage = $('#homePage');
+
+    button.addEventListener('click' ,()  => {
+        if (userLogin.value === "admin" && userPassword.value === "admin") {
+            loginPage.style.display = 'none';
+            homePage.style.display = 'flex';
+            document.title = 'Compass - Home';
+
+            logicalTimer();
+        } else {
+            messageError.style.visibility = 'visible';
+            userLogin.value = '';
+            userPassword.value = '';
+            inputLogin.style.border = '1px solid #E9B425';
+            inputPassword.style.border = '1px solid #E9B425';
+        }
+    });
+};   
